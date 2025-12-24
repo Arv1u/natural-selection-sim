602916a6dae2b80d11932c12369855eb778b756a
Arviu <arviu23@gmail.com>
2025-12-24 22:31:12 +0100

    Implemented new table user controls

---
+++ source/Natural Selection Sim/Natural Selection Sim/UserControls/TableHeader.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Natural_Selection_Sim.UserControls
+{
+    /// <summary>
+    /// Interaction logic for TableHeader.xaml
+    /// </summary>
+    public partial class TableHeader : UserControl
+    {
+        public TableHeader()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public static readonly DependencyProperty HeaderNameProperty = DependencyProperty.Register("HeaderName", typeof(string), typeof(TableHeader));
+        public string HeaderName 
+        {
+            get => (string)GetValue(HeaderNameProperty);
+            set => SetValue(HeaderNameProperty,value);
+        }
+    }
+}
